703624e827cd4d048c6558016394f221853a4261
tahariabegum <taharia97@gmail.com>
2024-01-29 01:01:02 -0500

    Login backend functionality

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -7,6 +7,7 @@ function generateToken(newUser) {
     return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 300 })
 }
 
+// Registration 
 async function register ( req, res ) {
 
     try {
@@ -33,6 +34,36 @@ async function register ( req, res ) {
     }
 }
 
+// Login 
+async function login( req, res ) {
+    
+    try {
+        // Check is username exists 
+        const foundUser = await User.findOne({ username: req.body.username })
+
+        if (!foundUser) {
+            return res.status(400).json({ error: "Username does not exist" })
+        }
+
+        // Check for a match in username & password 
+
+        const validPass = await bcrypt.compare(req.body.password, foundUser.password)
+
+        if (!validPass) {
+            return res.status(400).json({ error: 'Invalid username or password' })
+        }
+
+        // Generate JWT token and return to user 
+
+        const token = generateToken(foundUser)
+        res.status(200).json({ token })
+
+    } catch (err) {
+        res.status(200).json({ error: err.message })
+    }
+}
+
 module.exports = {
-    register
+    register, 
+    login
 }
