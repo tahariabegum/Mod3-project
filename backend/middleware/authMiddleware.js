f6042c872c2697b995f97960a347d16e30796c2b
tahariabegum <taharia97@gmail.com>
2024-01-29 01:16:02 -0500

    auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,39 @@
+const jwt = require ('jsonwebtoken')
+
+function authorize  (req, res, next) {
+
+    try {
+
+        // Check if the request has a JWT token 
+        let token = req.header("Authorization")
+
+        if (!token) {
+            return res.status(400).json({ error: "No token" })
+        }
+
+        // Check if token is valid and not expired 
+
+        const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+        if (payload.error) {
+            return res.status(400).json({ error: payload.error })
+        }
+
+        // Attach the payload from the token to request object 
+
+        req.id = payload.id 
+        req.username = payload.username 
+
+        // Next route 
+
+        next()
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(400).json({ error: err.message })
+    }
+}
+
+module.exports = {
+    authorize 
+}
