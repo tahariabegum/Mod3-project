d76d3dc0951cf42a400f2041e90fb20f08484c59
tahariabegum <taharia97@gmail.com>
2024-02-01 03:22:25 -0500

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/auth': 'http://localhost:8080', 
-      '/api': 'http://localhost:8080'
+      // '/auth': 'http://localhost:8080', 
+      // '/api': 'http://localhost:8080'
+      '/auth' : 'https://book-nook-backend.onrender.com',
+      '/api' : 'https://book-nook-backend.onrender.com'
     }
   },
   plugins: [react()],
