76f6b1dadf8e1c5bdb4f600b8d00882fd5a2b208
tahariabegum <taharia97@gmail.com>
2024-02-01 03:37:12 -0500

    baseuRL

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import baseURL from './Api.js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
+      '/auth': baseURL,
+      '/api' : baseURL
       // '/auth': 'http://localhost:8080', 
       // '/api': 'http://localhost:8080'
-      '/auth' : 'https://book-nook-backend.onrender.com',
-      '/api' : 'https://book-nook-backend.onrender.com'
+      // '/auth' : 'https://book-nook-backend.onrender.com',
+      // '/api' : 'https://book-nook-backend.onrender.com'
     }
   },
   plugins: [react()],
